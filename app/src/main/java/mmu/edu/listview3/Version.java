a5c23a601c847791fa23f73718d551f153de15d6
Naas Elhashmi <alnaash899@gmail.com>
2020-12-27 11:05:14 +0800

    tut1 & 2

---
+++ app/src/main/java/mmu/edu/listview3/Version.java
@@ -0,0 +1,26 @@
+package mmu.edu.listview3;
+
+public class Version {
+    private String name;
+    private String description;
+    private int icon;
+
+    public Version(String name, String description, int icon) {
+        this.name = name;
+        this.description = description;
+        this.icon = icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+}
+
